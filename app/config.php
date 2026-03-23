66603187c6641f01b6eb67e1b58c3eb8664509bd
littlebungalow <littlebungalowwebdev@gmail.com>
2026-03-23 20:49:40 +0000

    emai corrections

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'db_path' => __DIR__ . '/../data/app.db',
-    'studio_email' => 'nellysnailssss@gmail.com',
-    'from_email' => 'nellysnailssss@gmail.com',
+    'studio_email' => 'eleanorangharad@icloud.com',
+    'from_email' => 'eleanorangharad@icloud.com',
     'from_name' => "Nellys Nails",
     'timezone' => 'England/London',
     'send_status_emails' => true,
